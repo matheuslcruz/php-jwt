a1866c3837e49027bf16f6c14fc891c8d1114730
Matheus Lorenzoni Cruz <mlcruz@hcpa.edu.br>
2019-11-27 11:19:18 -0200

    initial commit

---
+++ sign.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once 'common.php';
+
+// Conf
+$conf = parse_ini_file('jwt.conf');
+
+// Header
+$header = ['typ' => 'JWT', 'alg' => 'HS256'];
+
+// Payload
+$payload = json_decode(file_get_contents('php://input'));
+$payload->iss = $conf['issuer'];
+$payload->exp = time() + ((int) $conf['token_exp']);
+
+// JSON
+$header = json_encode($header);
+$payload_json = $payload;
+$payload = json_encode($payload);
+
+// Base 64
+$header = base64url_encode($header);
+$payload = base64url_encode($payload);
+
+// Sign
+$sign = hash_hmac('sha256', $header . '.' . $payload, $conf['secret'], true);
+$sign = base64url_encode($sign);
+
+// Token
+$token = $header . '.' . $payload . '.' . $sign;
+
+header('Content-type: text/plain');
+echo $token;
+
+?>
---
+++ verify.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once 'common.php';
+
+// Conf
+$conf = parse_ini_file('jwt.conf');
+$token_parts = explode('.', $_GET['token']);
+
+if (count($token_parts) !== 3) {
+  header("HTTP/1.1 400 Invalid token");
+  header("Content-type: application/json");
+
+  echo json_encode([
+    'message' => 'Invalid token',
+    'token' => $_GET['token']
+  ]);
+} else {
+  $sign = hash_hmac('sha256', $token_parts[0] . '.' . $token_parts[1], $conf['secret'], true);
+  $sign = base64url_encode($sign);
+
+  if ($token_parts[2] !== $sign) {
+    header("HTTP/1.1 401 Invalid token");
+    header("Content-type: application/json");
+
+    echo json_encode([
+      'message' => 'Signature missmatch',
+      'token' => $_GET['token']
+    ]);
+  } else {
+    $payload = base64url_decode($token_parts[1]);
+    $payload_decoded = json_decode($payload);
+    $now = time();
+
+    if ((int) $payload_decoded->exp > (int) $now) {
+      header("Content-type: application/json");
+      echo $payload;
+    } else {
+      header("HTTP/1.1 401 Expired token");
+      header("Content-type: application/json");
+
+      echo json_encode([
+        'message' => "Token expired at ".date(DATE_RSS, $payload_decoded->exp),
+        'token' => $_GET['token']
+      ]);
+    }
+  }
+}
+
+?>
